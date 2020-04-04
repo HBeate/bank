a5a38b3b5ea9b91710ce84f7bba292d240e1703c
HBeate <hauser_b@hotmail.com>
2020-04-04 12:22:02 +0200

    sollte funktioniern

--- src/com/company/LoanAccount.java
+++ src/com/company/LoanAccount.java
@@ -5,4 +5,10 @@ public class LoanAccount extends  Account {
     public LoanAccount(double interestOwing, double interestEarned, double balance, double overdraftLimit) {
         super(interestOwing, interestEarned, balance, overdraftLimit);
     }
+
+    @Override
+    public void withdrawal(double withdrawalAmount) {
+        System.out.println("This is a loan. Withdrawals not possible.");
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,15 @@ public class Main {
 
         Account account1 = new Account(.05, 0.01, 500, -100);
         SavingsAccount savings1 = new SavingsAccount(0, .01, 5000, -3000);
+        LoanAccount loanAccount1 = new LoanAccount(.01, .00, -200000, 0);
 
         System.out.println("ACCOUNT 1");
         account1.deposit(40);
-        account1.deposit(60);
         account1.withdrawal(700);
-
         System.out.println("Balance: " + account1.getBalance());
         System.out.println("Interest: " + account1.getInterest());
-        account1.setInterest(account1.getInterest());
-
-        System.out.println("Balance: " +  account1.getBalance());
-
         account1.deposit(1000);
         account1.setInterest(account1.getInterest());
-
         System.out.println("Balance: " + account1.getBalance());
 
         System.out.println("SAVINGS 1 ");
@@ -30,7 +24,6 @@ public class Main {
         savings1.printBalance("Hypo balance: ");
         savings1.printBalance("Raubeisen balance: ");
         savings1.withdrawal(4000);
-        savings1.withdrawal(1000);
         savings1.deposit(3000);
         savings1.withdrawal(4000);
         savings1.withdrawal(2000);
@@ -39,5 +32,12 @@ public class Main {
         System.out.println("Interest: " + savings1.getInterest());
         savings1.setInterest(savings1.getInterest());
         System.out.println("Balance " + savings1.getBalance());
+
+        System.out.println("LOAN ACCOUNT 1");
+        loanAccount1.getBalance();
+        loanAccount1.withdrawal(500);
+        loanAccount1.deposit(100000);
+        loanAccount1.setInterest(loanAccount1.getInterest());
+        System.out.println("Balance: " + loanAccount1.getBalance());
     }
 }
